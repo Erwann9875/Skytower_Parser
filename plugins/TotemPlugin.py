2eb2a95690dd34ac5b163e8838ede23e014803b0
Erwann <erwann.tanguy@epitech.eu>
2025-07-01 14:10:00 +0200

    fix: multiple totem parsing

--- plugins/TotemPlugin.py
+++ plugins/TotemPlugin.py
@@ -6,19 +6,28 @@ class TotemPlugin(Plugin):
         self.lua = ""
 
     async def process(self, cleaned_lines):
-        totem = next((line for line in cleaned_lines if line[0] == "in" and line[1] == "2" and line[2] == "1457"), None)
+        totems = [line for line in cleaned_lines if line[0] == "in" and line[1] == "2" and line[2] == "1457"]
         lever = next((line for line in cleaned_lines if line[0] == "in" and line[1] == "9"), None)
 
-        if totem and lever:
-            vnum = totem[2]
-            x = totem[4]
-            y = totem[5]
-            direction = totem[6]
+        if totems:
+            self.lua = "map_1.AddNpcs({\n"
             
-            self.lua = f"""map_1.AddNpcs({{
-    MapNpc.CreateNpcWithVnum({vnum}).At({x}, {y}).Facing({direction}),
-}})\n"""
-            self.lua += f"""map_1.AddObjects({{
+            for i, totem in enumerate(totems):
+                vnum = totem[2]
+                x = totem[4]
+                y = totem[5]
+                direction = totem[6]
+                
+                self.lua += f"    MapNpc.CreateNpcWithVnum({vnum}).At({x}, {y}).Facing({direction})"
+                
+                if i < len(totems) - 1:
+                    self.lua += ","
+                self.lua += "\n"
+            
+            self.lua += "})\n"
+            
+            if lever:
+                self.lua += f"""map_1.AddObjects({{
     MapObject.CreateLever().At({lever[4]}, {lever[5]}).OnTrigger({{
     }}),
 }})"""
